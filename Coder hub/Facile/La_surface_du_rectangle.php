aea586885ad0111031788310c8513bdd85d8d2d9
abdo0302 <abdelljabarhamri06@gmail.com>
2024-10-11 13:54:33 +0100

    La_surface_du_rectangle

---
+++ Coder hub/Facile/La_surface_du_rectangle.php
@@ -0,0 +1,14 @@
+<!-- Description du défi :
+Écrivez une fonction qui prend deux entiers positifs représentant la longueur et la largeur d'un rectangle. La fonction doit calculer la surface du rectangle et retourner le résultat. -->
+
+
+<?php
+
+
+function area_of_rectangle($width, $height) {
+    return $width*$height;
+
+}
+echo area_of_rectangle(9,8)
+
+?>
