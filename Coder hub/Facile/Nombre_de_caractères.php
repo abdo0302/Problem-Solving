f81235d2b815e45924f7283bf8fb01bf774e8944
abdo0302 <abdelljabarhamri06@gmail.com>
2024-10-12 11:22:31 +0100

    Nombre de caractères

---
+++ Coder hub/Facile/Nombre_de_caractères.php
@@ -0,0 +1,13 @@
+<!-- Voici une fonction en JavaScript qui prend une chaîne en entrée et renvoie le nombre de caractères et d'espaces dans cette chaîne. -->
+
+
+<?php
+
+
+function name_char_length($name) {
+    return strlen($name);
+
+}
+
+
+?>
