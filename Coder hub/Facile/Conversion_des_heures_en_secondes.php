a793f8b5a28fa1879bcc5c430d8a15f1ba5bff24
abdo0302 <abdelljabarhamri06@gmail.com>
2024-10-15 22:00:20 +0100

    Conversion des heures en secondes

---
+++ Coder hub/Facile/Conversion_des_heures_en_secondes.php
@@ -0,0 +1,14 @@
+<!-- "Description du défi : Écrivez une fonction qui prend un nombre entier positif représentant le nombre d'heures. La fonction doit convertir le nombre d'heures en secondes et retourner le résultat." -->
+
+
+
+<?php
+
+
+function convert_to_seconds($hours) {
+    return $hours*3600;
+
+}
+
+
+?>
