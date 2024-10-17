61f7a21a5b1b2d352be024f56f1d1f0dcb676853
abdo0302 <abdelljabarhamri06@gmail.com>
2024-10-17 21:18:31 +0100

    La_soustraction_des_éléments_de_deux_matrices

---
+++ Coder hub/Moyen/La_soustraction_des_éléments_de_deux_matrices.php
@@ -0,0 +1,18 @@
+<!-- Description du défi :
+Écrire une fonction qui prend deux matrices de type entier (integer) en entrée. La fonction doit soustraire les éléments de la première matrice des éléments de la deuxième matrice, puis renvoyer une matrice de type entier (integer). -->
+
+
+
+<?php
+
+
+function sub_arrays($arr1, $arr2) {
+    $Soust=[];
+    for ($i=0; $i <count($arr1) ; $i++) { 
+        array_push($Soust,$arr1[$i]-$arr2[$i]);
+    }
+    return $Soust;
+}
+
+
+?>
